fadfcd894bd2782be065f61b21428879d448024f
ksed <hydrogeo.coder@gmail.com>
2016-11-08 19:41:19 -0500

    implemented PUT method for one todo

--- server/routers/todo.router.js
+++ server/routers/todo.router.js
@@ -9,7 +9,7 @@ router.use(bodyParser.urlencoded({extended: true}));
 
 // Our 5 RESTfull routing methods
 router.get('/todos', function(req, res) {
-  Todo.find({}, function(err, foundTodos){
+  Todo.find({}, function(err, foundTodos) {
     if(err) {
       res.status(500).json({
         err: err
@@ -21,7 +21,7 @@ router.get('/todos', function(req, res) {
   });
 });
 router.get('/todos/:id', function(req, res) {
-  Todo.find({_id: req.params.id}, function(err, foundTodo){
+  Todo.find({_id: req.params.id}, function(err, foundTodo) {
     if(err) {
       res.status(500).json({
         err: err
@@ -49,7 +49,16 @@ router.post('/todos', function(req, res) {
   //res.send(req.body);
 });
 router.put('/todos/:id', function(req, res) {
-
+  Todo.findOneAndUpdate({_id: req.params.id}, req.body, function(err, oldTodo) {
+    if(err) {
+      res.status(500).json({
+        err: err
+      });
+    }
+    res.status(200).json({
+      msg: oldTodo
+    });
+  });
 });
 router.delete('/todos/:id', function(req, res) {
   Todo.findOneAndRemove({_id: req.params.id}, function(err, deletedTodo) {
