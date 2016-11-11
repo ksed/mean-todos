07aa9ccc26cc7a31fc6e7f57352f36fdc70677cc
ksed <hydrogeo.coder@gmail.com>
2016-11-10 20:50:51 -0500

    implemented create method in todo service

--- public/js/services/todo.service.js
+++ public/js/services/todo.service.js
@@ -29,7 +29,13 @@
       return todos;
     }
     function createOneTodo(todo) {
-
+      $http.post('/todos', todo)
+          .then(function(response) {
+            todos.push(todo);
+          })
+          .catch(function(err) {
+            console.log(err);
+          });
     }
     function updateOneTodo(index, todo) {
 
