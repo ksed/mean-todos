75e72a8ac076fd45b340c5c698b492f69649a3a2
ksed <hydrogeo.coder@gmail.com>
2016-11-10 20:33:15 -0500

    implemented get all todos service and controller for front end

--- public/js/services/todo.service.js
+++ public/js/services/todo.service.js
@@ -3,11 +3,39 @@
   angular.module('mean-todos')
         .factory('TodoService', TodoService);
 
-  TodoService.$inject = [];
+  TodoService.$inject = ['$http'];
 
-  function TodoService(){
+  function TodoService($http){
+    init();
+    var todos = [];
     return {
-
+      get: getAllTodos,
+      create: createOneTodo,
+      update: updateOneTodo,
+      delete: deleteOneTodo
     };
+
+    function init() {
+      // this will make our first data request upon load
+      $http.get('/todos')
+          .then(function(response) {
+              todos = response.data.todos;
+            })
+          .catch(function(err) {
+            console.log(err);
+          });
+    }
+    function getAllTodos() {
+      return todos;
+    }
+    function createOneTodo(todo) {
+
+    }
+    function updateOneTodo(index, todo) {
+
+    }
+    function deleteOneTodo(index) {
+
+    }
   }
 }());
